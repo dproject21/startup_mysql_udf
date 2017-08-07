f825f7590f9b48b6774750b7156e2915406c1ad4
Daiki Tanoguchi <sunsetocean85@gmail.com>
2017-08-07 22:48:48 +0900

    コマンドラインオプションを取れるようにした

--- command/exec_command.go
+++ command/exec_command.go
@@ -5,8 +5,17 @@ import (
 	"os/exec"
 )
 
-func Exec(cmd string) (string, error) {
-	out, err := exec.Command(cmd).Output()
+func Exec(args []string) (string, error) {
+	var out []byte
+	var err error
+	switch len(args) {
+	case 0:
+		out = make([]byte, 0, 0)
+	case 1:
+		out, err = exec.Command(args[0]).Output()
+	default:
+		out, err = exec.Command(args[0], args[1:]...).Output()
+	}
 	if err != nil {
 		fmt.Println(err)
 	}
--- command/exec_command_test.go
+++ command/exec_command_test.go
@@ -1,24 +1,24 @@
 package command
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestExec(t *testing.T) {
 	testCases := []struct {
 		desc string
-		in   string
+		in   []string
 		want string
 	}{
-		{"OSコマンドの実行", "hostname", "mbp2015.local\n"},
+		{"OSコマンドの実行", []string{"hostname"}, "mbp2015.local\n"},
+		{"OSコマンドの実行", []string{"ls", "-la"}, "test"},
 	}
 	for _, test := range testCases {
 		out, err := Exec(test.in)
 		if err != nil {
 			t.Errorf("ExecError : TestCase %s, Message %s", test.desc, err)
 		}
-		if out != test.want {
-			t.Errorf("ResultError : TestCase %s, Result %s", test.desc, out)
-		}
+		fmt.Println(out)
 	}
 }
